47c6ab4beca9c1ec66eeb6d619acf1975b625027
Baylar55 <bylrsadiqov@gmail.com>
2024-08-14 23:36:00 +0400

    Initial Create

---
+++ API/Program.cs
@@ -0,0 +1,17 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StoreContext>(opt=>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
