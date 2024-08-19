fc42a31a034b18dd3197c4f6346e5eb15e0cbea5
Baylar55 <bylrsadiqov@gmail.com>
2024-08-19 09:36:54 +0400

    Product API completed

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -37,5 +37,36 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return product;
         }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Update(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id)) return BadRequest("Cannot update that Product");
+
+            _context.Entry(product).State=EntityState.Modified;
+            
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if(product is null) return NotFound();
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(x=>x.Id==id);
+        }
     }
 }
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27bc4598e179dcfd822db40f315f1db8ae14ded1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a14580bce3dacc61bda53f315fb63bb313ac68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
