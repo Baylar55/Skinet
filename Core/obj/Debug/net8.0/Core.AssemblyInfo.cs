e660a7b41e12fab0d095546c62e88d16625efc39
Baylar55 <bylrsadiqov@gmail.com>
2024-08-26 11:39:19 +0400

    Routing and Product Detail UI added

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a22c2924e92d46d71284e931711fb8d7483382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daad839296fb2c07d2a9d5b0fb1a935139420703")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
