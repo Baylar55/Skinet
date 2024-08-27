36208dca719c2394bc4b14f8d18d6d582fdd565a
Baylar55 <bylrsadiqov@gmail.com>
2024-08-27 23:03:37 +0400

    Client side Error Handling added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daad839296fb2c07d2a9d5b0fb1a935139420703")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e660a7b41e12fab0d095546c62e88d16625efc39")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
