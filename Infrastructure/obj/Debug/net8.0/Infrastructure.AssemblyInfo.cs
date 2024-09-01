8a43f71ac7214d78713d3b443eee943dda2058d5
Baylar55 <bylrsadiqov@gmail.com>
2024-09-01 23:26:01 +0400

    Shop UI added

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c01d4b6c835cf045571ca1f9ff5966c2532c74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b55cfa191ea79b639f341276fbf9b7e9f554c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
