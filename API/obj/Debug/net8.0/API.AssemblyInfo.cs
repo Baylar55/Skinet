fd005fb02e1ad01a539d8a70e824dd95895eae89
Baylar55 <bylrsadiqov@gmail.com>
2024-09-05 20:40:34 +0400

    Client side Regsiter Form and Form Validation added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c3e795f9f0f416f4480f4909969854e1f92b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e837416e08ff96042bd188bb9c5f9b4848e82fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
