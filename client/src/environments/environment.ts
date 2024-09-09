84db3a49f7553d439a12feae0d9d3f3037e9f340
Baylar55 <bylrsadiqov@gmail.com>
2024-09-09 23:38:15 +0400

    Adding client side Stripe

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey: 'pk_test_51PwpWDLXtd3RyHJM2v0z222zOFS4AMJyGWn52d7EBFInV9FErItZWGm4Jb64gwS4Wt4e3KN66HIKdOGpksjjPtY500L8AoMMuo',
 };
