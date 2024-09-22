beaac8a8131420657e811727f2828ddfaea6d2aa
Baylar55 <bylrsadiqov@gmail.com>
2024-09-22 23:51:09 +0400

    Prepared the client side for publishing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35680529304db5559a896a4783808d6b6cffdb14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e55f400653fb1d723f8c1edd3af586858c8de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
