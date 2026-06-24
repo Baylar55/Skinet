f44f7180b31947e49f0f5138939cef1c29d6f4fa
Baylar55 <bylrsadiqov@gmail.com>
2026-06-24 14:36:42 +0400

    fix(security): upgrade Angular to 21.2.17 and obfuscate APP_ID to mitigate CVE-2026-54267 (DOM Clobbering) - Bump @angular/core and all Angular packages from 21.2.4 to patched version 21.2.17 - Add obfuscated APP_ID ('app-skinet-<random>') to change the state script element ID from predictable 'ng-state' to a unique value - Enable provideClientHydration() in app config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_ID, APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
@@ -21,6 +22,8 @@ function initializeApp(initService: InitService){
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_ID, useValue: 'app-skinet-' + Math.random().toString(36).substring(2, 9) },
+    provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
